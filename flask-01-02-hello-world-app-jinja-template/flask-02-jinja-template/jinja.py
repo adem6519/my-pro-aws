8496cbe48542b6afe22593659b7b69499eaf2eb5
adym <adym6519@gmail.com>
2023-05-09 17:03:58 +0300

    flash-desk

--- flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
@@ -9,5 +9,5 @@ def number():
     y=20
     return render_template('body.html', num1=x, num2=y, mult=x*y)
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host= '0.0.0.0', port=80)
+    app.run(debug=True)
+    # app.run(host= '0.0.0.0', port=80)
